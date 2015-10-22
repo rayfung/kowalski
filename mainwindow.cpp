745787baf62aa40b782f4e6f57c9f09ed25fb62f
Ray Fung <posixfung@163.com>
2015-10-22 09:17:17 +0800

    基本实现所有功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,88 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QProcess>
+#include <QString>
+#include <QStringList>
+#include <QByteArray>
+#include <QInputDialog>
+#include <QMessageBox>
+
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->on_pushButtonRefresh_clicked();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QString MainWindow::RunProgram(const QString &path, const QStringList &arg)
+{
+    QProcess process;
+
+    process.start(path, arg);
+    if(!process.waitForStarted())
+        return QString("Cannot start program: " + path);
+    if(!process.waitForFinished())
+        return QString("Program not responding: " + path);
+    return QString::fromLocal8Bit(process.readAll());
+}
+
+void MainWindow::on_pushButtonRefresh_clicked()
+{
+    QString output = RunProgram("netsh", QStringList() << "wlan" << "show" << "hostednetwork");
+
+    ui->plainTextEditInfo->clear();
+    ui->plainTextEditInfo->appendPlainText(output);
+}
+
+void MainWindow::on_pushButtonStart_clicked()
+{
+    QString output = RunProgram("netsh", QStringList() << "wlan" << "start" << "hostednetwork");
+
+    ui->plainTextEditInfo->clear();
+    ui->plainTextEditInfo->appendPlainText(output);
+}
+
+void MainWindow::on_pushButtonStop_clicked()
+{
+    QString output = RunProgram("netsh", QStringList() << "wlan" << "stop" << "hostednetwork");
+
+    ui->plainTextEditInfo->clear();
+    ui->plainTextEditInfo->appendPlainText(output);
+}
+
+void MainWindow::on_pushButtonSettings_clicked()
+{
+    QString ssid = QInputDialog::getText(this, QString("设置 SSID"), QString("请输入热点的 SSID"),
+                                         QLineEdit::Normal, QString(), 0, 0, Qt::ImhLatinOnly);
+    if(ssid.trimmed().isEmpty())
+        return;
+
+    QString passphrase = QInputDialog::getText(this, QString("设置密码"), QString("请输入密码（8 至 32 个字符）"),
+                                               QLineEdit::Password, QString(), 0, 0, Qt::ImhLatinOnly);
+    if(passphrase.isNull())
+        return;
+    if(passphrase.length() < 8 || passphrase.length() > 32)
+    {
+        QMessageBox::information(this, QString("设置密码"), QString("密码不正确！"));
+        return;
+    }
+
+    QString output = RunProgram("netsh", QStringList() << "wlan" << "set" << "hostednetwork"
+                                << "mode=allow"
+                                << QString("ssid=%1").arg(ssid)
+                                << QString("key=%1").arg(passphrase)
+                                );
+    ui->plainTextEditInfo->clear();
+    ui->plainTextEditInfo->appendPlainText(output);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,4 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
@@ -14,6 +14,15 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    QString RunProgram(const QString &path, const QStringList &arg);
+
+private slots:
+    void on_pushButtonStart_clicked();
+    void on_pushButtonStop_clicked();
+    void on_pushButtonSettings_clicked();
+    void on_pushButtonRefresh_clicked();
 
 private:
     Ui::MainWindow *ui;
